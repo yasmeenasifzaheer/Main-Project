7fb283502c1aef6ad369d3318b6fb1e418eb1c93
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-03-10 23:21:11 +0530

    modified file

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Not Found" });
 });
 
+app.get("/", (req, res) => {
+  res.send("Movie Review API is running");
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
