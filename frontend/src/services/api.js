4ed9912b0860cd55ebe166fe12b91ca5ceef1ae9
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-03-17 15:54:44 +0530

    modified file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://main-project-1-20ny.onrender.com"
+  baseURL: "https://main-project-1-20ny.onrender.com/api"
 });
 
 export default API;
